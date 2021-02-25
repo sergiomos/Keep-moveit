783865ec19e9324185960c672b3a932494ec7cd7
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-25 10:10:35 -0300

    Fazendo componente de countdown e desafios

---
+++ src/components/Profile.tsx
@@ -0,0 +1,16 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://avatars.githubusercontent.com/u/39865942?s=460&u=926d9a6ae5649085886ce2584fd924e947ad22f7&v=4" alt="Sérgio Martins"/>
+      <div>
+        <strong>Sérgio Martins</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level"/>
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
