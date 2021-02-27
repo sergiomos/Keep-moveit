8dd0f0e7428d45c2f2ef77d69e61bd58fe97fc8c
Sérgio Martins <sergiomos2010@gmail.com>
2021-02-27 18:39:16 -0300

    dia 3 e 4

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,28 +1,44 @@
-import styles from '../styles/components/ChallengeBox.module.css'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import { CountdownContext } from '../contexts/CountdownContext';
+import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge, completChallenge } = useContext(ChallengesContext);
+  const { resetCountdown } = useContext(CountdownContext);
+
+  function handleChallengeSucceded() {
+    completChallenge();
+    resetCountdown();
+  }
+
+  function handleChallengeFailed() {
+    resetChallenge();
+    resetCountdown();
+  }
 
   return (
     <div className={styles.challengeBoxDontainer}>
-      { hasActiveChallenge ? (
+      { activeChallenge ? (
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
           <main>
-            <img src="icons/body.svg"/>
+            <img src={`icons/${activeChallenge.type}.svg`}/>
             <strong>Novo desafio</strong>
-            <p>Faça uma caminhade de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
           <footer>
             <button
               type='button'
               className={styles.challengeFailedButton}
+              onClick={handleChallengeFailed}
             >
               Falhei
             </button>
             <button
               type='button'
               className={styles.challengeSucceededButton}
+              onClick={handleChallengeSucceded}
             >
               Completei
             </button>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
-import styles from '../styles/components/Profile.module.css'
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const {level} = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://avatars.githubusercontent.com/u/39865942?s=460&u=926d9a6ae5649085886ce2584fd924e947ad22f7&v=4" alt="Sérgio Martins"/>
@@ -8,7 +12,7 @@ export function Profile() {
         <strong>Sérgio Martins</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
